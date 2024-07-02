9f92e313ca2968d208458352dc9eba02c75f4f49
farheen015 <nfarheen514@gmail.com>
2024-07-03 01:12:19 +0530

    project done

---
+++ data_dump.py
@@ -0,0 +1,27 @@
+import pymongo # pip install pymongo
+import pandas as pd
+import json
+
+
+client = pymongo.MongoClient("mongodb+srv://sunny022:sunny1997@cluster0.v0olynr.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+
+
+
+DATA_FILE_PATH = (r"F:\insurance_prediction\insurance.csv")
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
